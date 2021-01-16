6905e201f47bb84e408f6c29b1d5a153d4715319
Michael Ebenal <willtreatyaraluen@gmail.com>
2021-01-15 23:20:41 -0700

    Made the ICE group project look hecka good.

--- scripts/page.js
+++ scripts/page.js
@@ -1,6 +1,5 @@
 function loadDates() {
-    let str = document.getElementById("fullYear").innerHTML;
     let d = new Date();
-    document.getElementById("fullYear").innerHTML = "&copy; " + d.getFullYear() + str;
+    document.getElementById("fullYear").innerHTML = "&copy; " + d.getFullYear();
     document.getElementById("lastModified").innerHTML = "Last Updated: " + document.lastModified;
 }
