ddecdc7c208b9eae3bab5e3743774ad0047cfab7
Michael Ebenal <willtreatyaraluen@gmail.com>
2021-01-15 21:43:46 -0700

    Lesson 2

---
+++ lesson2/scripts/page.js
@@ -0,0 +1,6 @@
+function loadDates() {
+    
+    let d = new Date();
+    document.getElementById("fullYear").innerHTML = "&copy; " + d.getFullYear();
+    document.getElementById("lastModified").innerHTML = "Last Updated: " + document.lastModified;
+}
