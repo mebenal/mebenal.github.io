1ecfb169a597eb6cb3fcde7df8b740fb0c979a02
Michael Ebenal <willtreatyaraluen@gmail.com>
2021-01-30 01:34:48 -0700

    Pretty much finished header and footer css.

--- lesson4/js/page.js
+++ lesson4/js/page.js
@@ -1,4 +1,8 @@
 window.addEventListener('load', ()=>{
+    let d = new Date();
+    document.getElementById("fullYear").innerHTML = "&copy; " + d.getFullYear();
+    document.getElementById("lastModified").innerHTML = "Last Updated: " + document.lastModified;
+
     const hambutton = document.querySelector(".ham");
     const mainnav = document.querySelector("#navigation");
 
