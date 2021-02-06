4623b7736f6567b21e4ffbb9d5187cc884cff203
mebenal <willtreatyaraluen@gmail.com>
2021-02-06 11:31:10 -0700

    Addd lesson 5 files

---
+++ lesson5/js/page.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', ()=>{
+    let d = new Date();
+
+    document.getElementById("fullYear").innerHTML = "&copy; " + d.getFullYear();
+    document.getElementById("lastModified").innerHTML = d.toLocaleDateString('en-US', {weekday: 'long'}) + ', ' + 
+                                                        d.getDay() + ' ' + 
+                                                        d.toLocaleDateString('en-US', {month: 'long'}) + ' ' +
+                                                        d.getFullYear();
+
+    const hambutton = document.querySelector(".ham");
+    const mainnav = document.querySelector("#navigation");
+    const shut = document.querySelector(".shut");
+
+    hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')},false);
+    shut.addEventListener('click', ()=> {document.getElementById('banner').style.display = "none";
+                                         document.getElementById('gray').style.display = 'none';});
+    window.onresize = ()=> {if(window.innerWidth>552) mainnav.classList.remove('responsive');};
+});
