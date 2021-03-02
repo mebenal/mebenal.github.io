276768da209a8fdb8df2680e30e9f80f7c526c55
Michael Ebenal <willtreatyaraluen@gmail.com>
2021-03-02 14:00:51 -0700

    Finish prophets.html

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,19 +10,17 @@ window.addEventListener('load', () => {
         const card = document.createElement('section');
         const h2 = document.createElement('h2');
         const p1 = document.createElement('p');
-        const p2 = document.createElement('p');
         const img = document.createElement('img');
         const div = document.createElement('div');
 
         h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
-        p1.textContent = `Date of Birth: ${prophets[i].birthdate}`;
-        p2.textContent = `Place of Birth: ${prophets[i].birthplace}`;
+        p1.textContent = `Date of Birth: ${prophets[i].birthdate}\n`
+          + `Place of Birth: ${prophets[i].birthplace}`;
         img.src = prophets[i].imageurl;
         img.alt = `${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`;
 
         card.appendChild(h2);
         card.appendChild(p1);
-        card.appendChild(p2);
         card.appendChild(div).appendChild(img);
 
         document.querySelector('div.cards').appendChild(card);
